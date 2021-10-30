33e97264fec6886797b005810a8251d6518a0d84
Pathika Sai Teja Reddy <55448908+sai-tej31@users.noreply.github.com>
2021-10-30 10:56:31 +0530

    Update prime-number.py

--- prime-number.py
+++ prime-number.py
@@ -1,15 +1,14 @@
-Number = 1
-
-while(Number <= 100):
+number = 1
+def prime(number):
     count = 0
     i = 2
-    
-    while(i <= Number//2):
+    while(i <= number//2):
         if(Number % i == 0):
             count = count + 1
             break
         i = i + 1
 
-    if (count == 0 and Number != 1):
-        print(" %d" %Number, end = '  ')
-    Number = Number  + 1
+    if (count == 0 and number != 1):
+        print(" %d" %number, end = '  ')
+    number = number  + 1
+prime(number)
