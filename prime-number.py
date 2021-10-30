03f68a838032c822353a9ae4fc089a6416e5d346
NameSake <harshitsoni97@gmail.com>
2021-10-30 10:49:36 +0530

    Create prime-number.py

---
+++ prime-number.py
@@ -0,0 +1,15 @@
+Number = 1
+
+while(Number <= 100):
+    count = 0
+    i = 2
+    
+    while(i <= Number//2):
+        if(Number % i == 0):
+            count = count + 1
+            break
+        i = i + 1
+
+    if (count == 0 and Number != 1):
+        print(" %d" %Number, end = '  ')
+    Number = Number  + 1
