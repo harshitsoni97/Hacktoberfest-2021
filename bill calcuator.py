b67833deb922ab43c536c3fc62315300235b7e52
vaishalithk22 <47034294+vaishalithk22@users.noreply.github.com>
2021-10-31 21:16:16 +0530

    Updated New Changes
    Updated New Changes

--- bill calcuator.py
+++ bill calcuator.py
@@ -1,10 +1,7 @@
 
 units = int(input(" Please enter Number of Units you Consumed : "))
 
-if(units < 50):
-    amount = units * 2.60
-    surcharge = 25
-elif(units <= 100):
+if(units <= 100):
     amount = 130 + ((units - 50) * 3.25)
     surcharge = 35
 elif(units <= 200):
